e7077a56e0279e56bf71fe6dcfb2fb25b4642e5a
prateek-ml <prateekb1912@gmail.com>
2021-05-14 15:57:04 +0530

    Add new Image classifier Activity

---
+++ app/src/main/java/com/criclytica/facemaskdetector/ImageClassifierActivity.kt
@@ -0,0 +1,10 @@
+package com.criclytica.facemaskdetector
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ImageClassifierActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
